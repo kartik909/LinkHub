da30bcba780273c62120e44631c5c671e17a99af
kartik patel <kartikspatel@gmail.com>
2018-04-30 22:29:57 -0500

    Designed Controllers, Actions and Added Bootstrap

---
+++ LinkHubUI/LinkHubUI/Areas/User/Controllers/URLController.cs
@@ -0,0 +1,28 @@
+﻿using BOL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LinkHubUI.Areas.User.Controllers
+{
+    public class URLController : Controller
+    {
+        // GET: User/URL
+        public ActionResult Index()
+        {
+            LinkHubDbEntities db = new LinkHubDbEntities();
+            ViewBag.CategoryId = new SelectList(db.tbl_Category,"CategoryId", "CategoryName");
+            return View();
+        }
+
+        // POST: User/URL
+        public ActionResult Create(tbl_Url objUrl)
+        {
+            //LinkHubDbEntities db = new LinkHubDbEntities();
+            //ViewBag.CategoryId = new SelectList(db.tbl_Category, "CategoryId", "CategoryName");
+            return View();
+        }
+    }
+}
