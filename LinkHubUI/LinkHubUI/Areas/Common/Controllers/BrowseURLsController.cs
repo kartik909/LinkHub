b9d0b8297c52f8da94cb67efedad5df14d07d7ad
kartik patel <kartikspatel@gmail.com>
2018-05-02 17:47:52 -0500

    Implemented Browse URLs

---
+++ LinkHubUI/LinkHubUI/Areas/Common/Controllers/BrowseURLsController.cs
@@ -0,0 +1,25 @@
+﻿using BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LinkHubUI.Areas.Common.Controllers
+{
+    public class BrowseURLsController : Controller
+    {
+        UrlBs objBs;
+
+        public BrowseURLsController()
+        {
+            objBs = new UrlBs();
+        }
+        // GET: Common/BrowseURL
+        public ActionResult Index()
+        {
+            var Urls = objBs.GetALL().Where(x=>x.IsApproved == "A").ToList();  
+            return View(Urls);
+        }
+    }
+}
