4e35d87ca43c343b22979a59c8094f0078c8944b
kartik patel <kartikspatel@gmail.com>
2018-05-07 16:01:04 -0500

    Implemented Approve Urls and UserRegistration

--- LinkHubUI/LinkHubUI/Areas/Admin/Controllers/ApproveURLsController.cs
+++ LinkHubUI/LinkHubUI/Areas/Admin/Controllers/ApproveURLsController.cs
@@ -6,12 +6,57 @@ using System.Web.Mvc;
 
 namespace LinkHubUI.Areas.Admin.Controllers
 {
-    public class ApproveURLsController : Controller
+    public class ApproveURLsController : BaseAdminController
     {
         // GET: Admin/ApproveURLs
-        public ActionResult Index()
+        public ActionResult Index(string Status)
         {
-            return View();
+            ViewBag.Status = (Status == null ? "P" : Status);
+            if (Status == null)
+            {
+                var urls = obj.urlBs.GetALL().Where(x => x.IsApproved == "P").ToList();
+                return View(urls);
+            }
+            else
+            {
+                var urls = obj.urlBs.GetALL().Where(x => x.IsApproved == Status).ToList();
+                return View(urls);
+            }
+            
+        }
+
+        public ActionResult Approve(int id)
+        {
+            try
+            {
+                var myUrl = obj.urlBs.GetByID(id);
+                myUrl.IsApproved = "A";
+                obj.urlBs.Update(myUrl);
+                TempData["Msg"] = "Approved";
+                return RedirectToAction("Index");
+            }
+            catch (Exception e1)
+            {
+                TempData["Msg"] = "Not Approved" +e1;
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Reject(int id)
+        {
+            try
+            {
+                var myUrl = obj.urlBs.GetByID(id);
+                myUrl.IsApproved = "R";
+                obj.urlBs.Update(myUrl);
+                TempData["Msg"] = "Rejected";
+                return RedirectToAction("Index");
+            }
+            catch (Exception e1)
+            {
+                TempData["Msg"] = "Not Approved" + e1;
+                return RedirectToAction("Index");
+            }
         }
     }
 }
--- LinkHubUI/LinkHubUI/Areas/Security/Controllers/RegisterController.cs
+++ LinkHubUI/LinkHubUI/Areas/Security/Controllers/RegisterController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BOL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +7,41 @@ using System.Web.Mvc;
 
 namespace LinkHubUI.Areas.Security.Controllers
 {
-    public class RegisterController : Controller
+    public class RegisterController : BaseSecurityController
     {
         // GET: Security/Register
         public ActionResult Index()
         {
+
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(tbl_User user)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    user.Role = "U";
+                    obj.userBs.Insert(user);
+                    TempData["Msg"] = "user Registered";
+                    return RedirectToAction("Index");
+                }
+
+                else
+                {
+                    return View("Index");
+                }
+            }
+            catch (Exception e1)
+            {
+                TempData["Msg"] = "Registration Failed" +e1;
+                return RedirectToAction("Index");
+
+            }
+                       
+
         }
     }
 }
